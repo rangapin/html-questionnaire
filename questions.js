e823c3e6a1922955ee0f3fb4b409072adef2ef83
Richard Angapin <rangapin@gmx.com>
2020-03-04 21:18:21 +0700

    Update and rename data.json to questions.js

---
+++ questions.js
@@ -0,0 +1,51 @@
+var questions = [
+	{
+	  q : "La norme HTML5 ne nécessite pas de guillemets autour des valeurs d'attribut:",
+	  o : [
+		"Vrai",
+		"Faux"
+	  ],
+      a : 0,
+      arg : "L'utilisation de guillemets est courante. L'omission de guillemets peut produire des erreurs."
+	},
+	{
+	  q : "L'élément &lt;div&gt; est un élément non sémantique:",
+	  o : [
+		"Vrai",
+		"Faux"
+	  ],
+      a : 0,
+      arg : "L’élément &lt;div&gt; n’indique rien sur son contenu."
+
+	},
+	{
+	  q : "Les éléments en ligne sont normalement affichés sans commencer une nouvelle ligne:",
+	  o : [
+		"Vrai",
+		"Faux"
+	  ],
+      a : 0,
+      arg : "Un élément en ligne ne démarre pas sur une nouvelle ligne et ne prend que la largeur nécessaire."
+
+	},
+	{
+	  q : "Un &lt;iframe&gt; est utilisé pour afficher une page Web dans une page Web:",
+	  o : [
+		"Vrai",
+		"Faux"
+	  ],
+      a : 0,
+      arg : "Un &lt;iframe&gt; est utilisé pour incorporer un autre document dans le document HTML actuel."
+
+	},
+	{
+	  q : "Les commentaires HTML commencent par <- et se terminent par ->:",
+	  o : [
+		"Vrai",
+		"Faux"
+	  ],
+      a : 0,
+      arg : "Les commentaires HTML commencent par &lt;!-- et se terminent par --&gt;"
+
+	}
+  ];
